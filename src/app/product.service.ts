9c7472c12498346e431442e6366cb63708245081
Jaron Diepgrond <J.Diepgrond@arcady.nl>
2020-02-03 15:50:17 +0100

    assignment 3

---
+++ src/app/product.service.ts
@@ -0,0 +1,17 @@
+import { Http, Response } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+// @Injectable({
+//   providedIn: 'root'
+// })
+export class ProductService {
+
+  _albumUrl: string =  '../assets/album.json';
+
+  constructor(private _http: Http) { }
+
+  getAlbum(id: number){
+    return this._http.get(this._albumUrl).map((response) =>  response.json());
+
+  }
+}
