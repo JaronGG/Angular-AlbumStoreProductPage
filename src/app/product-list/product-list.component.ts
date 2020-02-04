126e66a4f1c42c27dde5ba38d5f0e718744bf163
Jaron Diepgrond <J.Diepgrond@arcady.nl>
2020-02-04 08:45:52 +0100

    update

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -10,10 +10,10 @@ import { ProductService } from '../product.service';
 export class ProductListComponent implements OnInit {
 
   products: Product[];
-  constructor(private _productservice: ProductService) { }
+  constructor(private _productService: ProductService) { }
 
   ngOnInit() {
-    this._productservice.getProducts().subscribe(response => this.products = response);
+    this._productService.getProducts().subscribe(response => this.products = response);
   }
 
 }
